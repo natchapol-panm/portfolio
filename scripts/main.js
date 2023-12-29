4907be5741e5671be19ddf9a09ea136250ddcd7a
DESKTOP-393VC37\Asus  DesktopPc <natchapol.panm@bumail.net>
2023-12-29 09:57:22 +0700

    test commit

--- scripts/main.js
+++ scripts/main.js
@@ -452,35 +452,35 @@ function orientPlayer() {
   }
 }
 
-function initTouchEvents() {
-  document.addEventListener("touchstart", handleStart);
-  document.addEventListener("touchmove", handleMove);
-  document.addEventListener("touchend", handleEnd);
-}
-function handleStart(e) {
-  touchStartX = e.targetTouches[0].pageX;
-  pageVerticalPositionOnTouch = pageVerticalPosition;
-}
-function handleMove(e) {
-  e.preventDefault();
-  touchCurrentX = e.targetTouches[0].pageX;
+// function initTouchEvents() {
+//   document.addEventListener("touchstart", handleStart);
+//   document.addEventListener("touchmove", handleMove);
+//   document.addEventListener("touchend", handleEnd);
+// }
+// function handleStart(e) {
+//   touchStartX = e.targetTouches[0].pageX;
+//   pageVerticalPositionOnTouch = pageVerticalPosition;
+// }
+// function handleMove(e) {
+//   e.preventDefault();
+//   touchCurrentX = e.targetTouches[0].pageX;
 
-  if (canScrollOrSwipe === true) {
-    detectPageVerticalPosition();
-    runTheseFunctionsAfterScrollOrSwipe();
-  }
-}
-function handleEnd(e) {
-  e.preventDefault();
-  touchEndX = e.changedTouches[0].pageX;
-}
+//   if (canScrollOrSwipe === true) {
+//     detectPageVerticalPosition();
+//     runTheseFunctionsAfterScrollOrSwipe();
+//   }
+// }
+// function handleEnd(e) {
+//   e.preventDefault();
+//   touchEndX = e.changedTouches[0].pageX;
+// }
 
-$(window).on("beforeunload", function () {
-  $(window).scrollTop(0);
-});
+// $(window).on("beforeunload", function () {
+//   $(window).scrollTop(0);
+// });
 
 function onLoad() {
-  initTouchEvents();
+  // initTouchEvents();
   storeDivs();
   setPageHeight();
   setItemsSpeed();
